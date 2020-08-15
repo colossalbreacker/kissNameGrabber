e4a1106fd941a8fa7588e068f5cda4272f5c4037
colossalbreacker <josephrocket1@gmail.com>
2020-08-15 14:36:52 -0400

    Add project files.

---
+++ kissManga/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace kissManga
+{
+    partial class grabber
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(64, 269);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(283, 159);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Filter";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(64, 167);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(429, 35);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(64, 73);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(459, 63);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Insert csv location";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton1.Location = new System.Drawing.Point(520, 376);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(170, 52);
+            this.radioButton1.TabIndex = 3;
+            this.radioButton1.Text = "Anime";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Checked = true;
+            this.radioButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton2.Location = new System.Drawing.Point(520, 269);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(178, 52);
+            this.radioButton2.TabIndex = 4;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Manga";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 694);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(847, 58);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Use the chrome plugin linked below to export your google history as a csv file. \r" +
+    "\nThen insert the file location of the csv file into the text box and click the b" +
+    "utton.";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabel1.Location = new System.Drawing.Point(48, 806);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(1163, 29);
+            this.linkLabel1.TabIndex = 6;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "https://chrome.google.com/webstore/detail/export-chrome-history/dihloblpkeiddiaoj" +
+    "bagoecedbfpifdj/related";
+            this.linkLabel1.Click += new System.EventHandler(this.linkLabel1_Click);
+            // 
+            // grabber
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1231, 909);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "grabber";
+            this.Text = "Grab Kiss History";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
+
---
+++ kissManga/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+//using System.Linq;
+using System.Text;
+//using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kissManga
+{
+    public partial class grabber : Form
+    {
+        public grabber()
+        {
+            InitializeComponent();
+            names = new List<string>();
+        }
+        List<string> names;
+        private void button1_Click(object sender, EventArgs e) //shitty code that works
+        {
+            string temp = "";
+            string search = "";
+            string output = "";
+            if (radioButton1.Checked)
+            {
+                search = "kissanime.ru/Anime/";
+                output = "anime.txt";
+            }
+            else
+            {
+                search = "kissmanga.com/Manga/";
+                output = "manga.txt";
+            }
+            try
+            {
+                // var strLines = File.ReadLines(textBox1.Text);
+                //foreach (var line in strLines)
+                System.IO.StreamReader file = new StreamReader(textBox1.Text);
+                while ((temp = file.ReadLine()) != null)
+                {
+                    {
+                        //temp = line;
+                        int index = temp.IndexOf(search);
+                        if (index > -1)
+                        {
+                            temp = temp.Substring(index, temp.Length - index);
+                            string[] arr = temp.Split('/', ',', '?');
+                            arr[2] = arr[2].Replace('-', ' ');
+                            if (!names.Contains(arr[2]))
+                                names.Add(arr[2]);
+
+                        }
+
+                    }
+                }
+                    names.Sort();
+                    TextWriter tw = new StreamWriter(output);
+
+                    foreach (String s in names)
+                        tw.WriteLine(s);
+
+                tw.Close();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+      
+
+        private void linkLabel1_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://chrome.google.com/webstore/detail/export-chrome-history/dihloblpkeiddiaojbagoecedbfpifdj/related");
+        }
+    }
+}
